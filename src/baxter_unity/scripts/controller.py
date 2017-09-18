764bb4120d28b2d2bfa5b3151e1d43f2c5ecc22f
Thejana Satanarachchi <thejanasatanarachchi@outlook.com>
2017-09-18 16:21:52 +1000

    message passing to control node from unity

--- src/baxter_unity/scripts/controller.py
+++ src/baxter_unity/scripts/controller.py
@@ -1,49 +1,132 @@
 #!/usr/bin/env python
 
-import argparse, sys
-from multiprocessing import Process
-from Queue import Queue
+import rospy
+from multiprocessing import JoinableQueue, Process
 
-from baxter_unity import WSServer
-from baxter_unity import BaxterNode
+"""
+Baxter Interface classes for easy access to ROS API
+"""
+from baxter_interface import (
+    Limb,
+    RobotEnable,
+)
 
-def start_node_server(host, port):
-    
-    # Control Message Queue and Cancel Message Queue
-    control_queue = Queue()
-    cancel_queue = Queue()
+"""
+Geometry messages required for the IKSolver Service
+"""
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Header,
+) 
 
-    # setup concurrent process for ws_server
-    ws = WSServer(control_queue, cancel_queue, host, port)
-    ws_process = Process(target=ws.listen, args=())
-    
-    # setup concurrent process for control_node
-    cn = BaxterNode(control_queue, cancel_queue)
-    cn_process = Process(target=cn.start, args=())
+from baxter_core_msgs.msg import (
+    JointCommand,
+    EndpointState,
+)
 
-    ws_process.start()
-    cn_process.start()
+from sensor_msgs.msg import (
+    JointState,
+)
 
-def main():
-    """
-    Argument Parser to process the arguments passed into the controller
-    """
-    arg_fmt = argparse.ArgumentDefaultsHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt)
+"""
+Baxter IK Solver
+"""
+from baxter_core_msgs.srv import (
+    SolvePositionIK as ik_solver
+)
 
-    parser.add_argument(
-        "-p", "--port", dest="port", default="5005", 
-        help="Port that the Websocket server is listening on"
-    )
+import time
 
-    parser.add_argument(
-        "-h", "--host", dest="host", default="0.0.0.0",
-        help="IP that the Websocket server is listening on"
-    )
+class BaxterNode():
+    def __init__(self, con_queue, can_queue):
+        self.control_queue = con_queue
+        self.cancel_queue = can_queue
+        self.node = 'rsdk_baxter_unity'
 
-    args = parser.parse_args(rospy.myargv()[1:])
-    start_node_server(args.host, args.port)
+        self._component_ids = {
+            'left': ['left_e0', 'left_e1', 'left_s0', 'left_s1', 'left_w0', 'left_w1', 'left_w2'],
+            'right': ['right_e0', 'right_e1', 'right_s0', 'right_s1', 'right_w0', 'right_w1', 'right_w2']
+        }
 
+        # IKSolver topics - Accepts sensor_msgs/JointState
+        self.ik_topics = {
+            'left': '/ExternalTools/left/PositionKinematicsNode/IKService',
+            'right': '/ExternalTools/right/PositionKinematicsNode/IKService',
+        }
 
-if __name__ == '__main__':
-    sys.exit(main())
+        # JointCommand topics - Accepts baxter_core_msgs/JointCommand
+        self.comm_topics = {
+            'left': '/robot/limb/left/joint_command',
+            'right': '/robot/limb/right/joint_command'
+        }
+
+        rospy Publishers for IKService topics
+        self.ik_publishers = {
+            'left': rospy.Publisher(
+                self.ik_topics['left'], 
+                sensor_msgs.msg.JointState, 
+                queue_size = None,
+                tcp_nodelay = True
+            ),
+            'right':  rospy.Publisher(
+                self.ik_topics['right'],
+                sensor_msgs.msg.JointState,
+                queue_size = None,
+                tcp_nodelay = True
+            )
+        }
+
+        # rospy Publishers for JointCommand topics
+        self.comm_publishers = {
+            'left': rospy.Publisher(
+                self.comm_topics['left'],
+                baxter_core_msgs.msg.JointCommand,
+                queue_size = None,
+                tcp_nodelay = True
+            ),
+            'right': rospy.Publisher(
+                self.comm_topics['right'],
+                baxter_core_msgs.msg.JointCommand,
+                queue_size = None,
+                tcp_nodelay = True
+            )
+        }
+
+    def sprint(self, message):
+        print('[' + self.node + '] %s' % message)
+
+    def cleanup(self):
+        self.control_queue = None
+        self.cancel_queue = None
+
+    def start(self):
+        self.sprint('starting node...')
+        rospy.init_node(self.node)
+
+        # Enable Baxter
+        robot = RobotEnable()
+        robot.enable()
+
+        rospy.on_shutdown(self.cleanup)
+        self.sprint('Node running - Ctrl + C to shutdown')
+        rospy.spin()
+        self.sprint('Control Node running...')
+        curr_time = time.time()
+        while(True):
+            if curr_time - time.time() == -1:
+                curr_time = time.time()
+                self._consume_cancel()
+                self._consume_control()
+
+    def _consume_control(self):
+        try:
+            self.sprint(self.control_queue.get_nowait())
+        except:
+            return
+
+    def _consume_cancel(self):
+        try:
+            self.sprint(self.cancel_queue.get_nowait())
+        except:
+            return
---
+++ src/baxter_unity/scripts/start_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import argparse, sys
+from multiprocessing import Process, Queue
+
+import wsserver
+import controller
+
+def start_node_server(host, port):
+    
+    # Control Message Queue and Cancel Message Queue
+    control_queue = Queue()
+    cancel_queue = Queue()
+
+    # setup concurrent process for ws_server
+    ws = wsserver.WSServer(control_queue, cancel_queue, host, port)
+    ws_process = Process(target=ws.listen, args=())
+    
+    # setup concurrent process for control_node
+    cn = controller.BaxterNode(control_queue, cancel_queue)
+    cn_process = Process(target=cn.start, args=())
+
+    cn_process.start()
+    ws_process.start()
+    cn_process.join()
+    ws_process.join()
+
+def main():
+    """
+    Argument Parser to process the arguments passed into the controller
+    """
+    arg_fmt = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=arg_fmt)
+
+    parser.add_argument(
+        "-p", "--port", dest="port", default="5005", 
+        help="Port that the Websocket server is listening on"
+    )
+
+    parser.add_argument(
+        "-s", "--host", dest="host", default="0.0.0.0",
+        help="IP that the Websocket server is listening on"
+    )
+
+    args = parser.parse_args(sys.argv[1:])
+    start_node_server(args.host, args.port)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
